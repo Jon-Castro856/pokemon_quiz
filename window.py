e858be24c38cd400552ed7b7057e59aea88baa1d
Jon Castro <jonattancast@gmail.com>
2025-05-31 11:56:19 -0400

    Christ, man

--- window.py
+++ window.py
@@ -1,19 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+from PIL import Image, ImageTk
 
 class Window:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.resizable(width= True, height= True)
-        self.root.title("Poke Quiz")
+        self.root.geometry('800x600')
+        bg = Image.open('images/bg.png')
 
-        #Set up style
-        s = ttk.Style()
-        s.configure('mainFrame.TFrame', background= "#ee1515")
+        bg = ImageTk.PhotoImage(bg)
+        bg_label = tk.Label(self.root, image= bg)
+        bg_label.place(x= 0, y= 0, relwidth= 1, relheight= 1)
+        bg_label.image = bg
+        start_bt = ttk.Button(self.root, text= 'Start')
+        quit_bt = ttk.Button(self.root, text= 'Quit', command= self.root.destroy)
 
-        #Render the frames
-        frm = ttk.Frame(self.root, width=800, height=600, padding=10, style= 'mainFrame.TFrame')
-        self.root.columnconfigure(index=0, weight=1)
-        self.root.rowconfigure(index=1, weight=1)
-        frm.grid(sticky='nsew')
+        start_bt.place(x= 355, y= 200)
+        quit_bt.place(x= 355, y= 400)
         self.root.mainloop()
